c66c2e7fc7cfc19eb1f2d6e902ccbc770de0494a
rbs5241 <31281219+rbs5241@users.noreply.github.com>
2018-03-18 14:42:31 -0400

    Update MainActivity.kt

--- main/MainActivity.kt
+++ main/MainActivity.kt
@@ -1,5 +1,7 @@
 package golf.golf_app_2
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,6 +10,8 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -29,6 +33,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+
+        configureVideoView() //RS - Configuring the video playback of animation
+        configureSeekBar()   //RS - Configuring the seek bar, see method for Listener
     }
 
     override fun onBackPressed() {                           //When back is pressed close the drawer
@@ -55,36 +63,84 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_swing_1 -> {
-                // Handle swing 1 button action
+            R.id.nav_swing1 -> {
+                val videoView1 = findViewById<VideoView>(R.id.videoView1) as VideoView
+                val sw1_path = "android.resource://" + getPackageName() + "/" + R.raw.swing1
+                videoView1.setVideoURI(Uri.parse(sw1_path))
             }
-            R.id.nav_swing_2 -> {
-                // Handle swing 2 button action
+            R.id.nav_swing2 -> {
+                val videoView1 = findViewById<VideoView>(R.id.videoView1) as VideoView
+                val sw2_path = "android.resource://" + getPackageName() + "/" + R.raw.swing2
+                videoView1.setVideoURI(Uri.parse(sw2_path))
             }
-            R.id.nav_swing_3 -> {
-                // Handle swing 3 button action
+            R.id.nav_slideshow -> {
             }
-            R.id.nav_swing_4 -> {
-                // Handle swing 4 button action
-            }
-            R.id.nav_swing_5 -> {
-                // Handle swing 5 button action
-            }
-            R.id.nav_swing_6 -> {
-                // Handle swing 6 button action
-            }
-            R.id.nav_swing_7 -> {
-                // Handle swing 7 button action
-            }
-            R.id.nav_settings -> {
-                // Handle settings button action
+            R.id.nav_manage -> {
             }
         }
-
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+    /* configurevideoview
+     * sets the path of the raw video,
+     * button1 - toggle play and pause
+     */
+    private fun configureVideoView() {
+        val videoView1 = findViewById<VideoView>(R.id.videoView1) as VideoView
+        val path = "android.resource://" + getPackageName() + "/" + R.raw.swing1       //the path for the video, in project tree under res directory create a raw directory and place video here
+        videoView1.setVideoURI(Uri.parse(path))                                        //setting the video uri
+        //videoView1.start()
+       // mediaController = MediaController(this)
+        //mediaController.setAnchorView(videoView1)
+        //videoView1.setMediaController(mediaController
+        //)
+        videoView1.requestFocus()
+        videoView1.setOnPreparedListener{ mp ->
+            mp.isLooping = true
+            //Log.i("Swing1", "Duration = " + videoView1.duration)
+
+            videoView1.start()
+        }
+
+        val button = findViewById<Button>(R.id.button1)
+        button.setText(R.string.pause)
+        button.setOnClickListener({
+            val isPlaying = videoView1.isPlaying
+            button.setText(if (isPlaying) R.string.play else R.string.pause)
+            val msg = getString(if (isPlaying) R.string.paused else R.string.playing)
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+
+            if(isPlaying){
+                videoView1.pause()
+            } else {
+                videoView1.start()
+            }
+        })
+    }
+    private fun configureSeekBar() {
+        val seekBar1 = findViewById<SeekBar>(R.id.seekBar1) as SeekBar
+        //seekBar1.max = 300
+        val videoView1 = findViewById<VideoView>(R.id.videoView1) as VideoView
+
+        seekBar1.setOnSeekBarChangeListener (object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar1:SeekBar, progress:Int, fromUser:Boolean ){
+
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // called when tracking the seekbar is started
+                videoView1.pause()
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // called when tracking the seekbar is stopped
+                Toast.makeText(this@MainActivity, "Progress is " + seekBar.progress + "%", Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+
     }
 }
